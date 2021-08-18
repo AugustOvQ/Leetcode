b19b6d2c39bc18a1e8f4e01b77e2db1539218fcb
Wangyuan Ding <dingwanyua@icloud.com>
2021-08-18 08:44:33 +0100

    Day2

--- Q1-100/011_Container_With_Most_Water.py
+++ Q1-100/011_Container_With_Most_Water.py
@@ -1,30 +1,38 @@
+"""
+Notes:
+    Start from brute force, noticing that the hight is min(x, y)
+    and the width is diff(x, y). So start from left most and right most
+    greedily.
+    
+Leetcode:
+    https://www.youtube.com/watch?v=UuiTKBwPgAo
+"""
 from typing import List
 
 
 class Solution:
     """
-    Simple two pointers:
-        Left most and right most, choose the higher one as height,
-        the difference of location as width
+    Brute force:
+        Check every combination, loop of depth 2
+
     Complexity:
-        O(n)
+        O(n^2), rejected by leetcode
     """
 
     def maxArea(self, height: List[int]) -> int:
-        left, right = 0, len(height) - 1
-        result = 0
-        while left < right:
-            result = max(min(height[left], height[right])
-                         * (right - left), result)
-            if height[left] > height[right]:
-                right -= 1
-            else:
-                left += 1
-        return result
+        res = 0
+
+        ls = len(height)
+        for l in range(ls):
+            for r in range(l+1, ls):
+                area = (r - l) * min(height[l], height[r])
+                res = max(res, area)
+
+        return res
 
 
 s = Solution()
-print("Simple greedy:")
+print("Brute force:")
 print(s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
 print(s.maxArea([1, 1]))
 print(s.maxArea([4, 3, 2, 1, 4]))
@@ -34,12 +42,29 @@ print(s.maxArea([1, 2, 1]))
 
 
 class Solution:
-    def maxArea(self, height):
-        res, l, r = 0, 0, len(height) - 1
+    """
+    Simple two pointers:
+        Start from left most and right most, shift the pointer leave the
+        pointer with lower height
+
+    Complexity:
+        O(n)
+    """
+
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+        ls = len(height)
+        l, r = 0, ls-1
+
         while l < r:
-            h = min(height[l], height[r])
-            res, l, r = max(res,  h * (r - l)), l + \
-                (height[l] == h), r - (height[r] == h)
+            area = (r - l) * min(height[l], height[r])
+            res = max(res, area)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
         return res
 
 
